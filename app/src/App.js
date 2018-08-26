c551e3a336119812d739def7ce44b57390fc416f
cynthialu42 <clu2334@gmail.com>
2018-08-26 13:03:31 -0500

    initial commit

---
+++ app/src/App.js
@@ -0,0 +1,131 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+// http://api.petfinder.com/pet.find?key=3eeae4b562e33e3abbbc4563617733fd&animal=bird&breed=Cockatiel&output=full&location=53719&format=json
+/*
+            "$t": "African Grey"
+                },
+                {
+                    "$t": "Amazon"
+                },
+                {
+                    "$t": "Brotogeris"
+                },
+                {
+                    "$t": "Budgie / Budgerigar"
+                },
+                {
+                    "$t": "Button-Quail"
+                },
+                {
+                    "$t": "Caique"
+                },
+                {
+                    "$t": "Canary"
+                },
+                {
+                    "$t": "Chicken"
+                },
+                {
+                    "$t": "Cockatiel"
+                },
+                {
+                    "$t": "Cockatoo"
+                },
+                {
+                    "$t": "Conure"
+                },
+                {
+                    "$t": "Dove"
+                },
+                {
+                    "$t": "Duck"
+                },
+                {
+                    "$t": "Eclectus"
+                },
+                {
+                    "$t": "Emu"
+                },
+                {
+                    "$t": "Finch"
+                },
+                {
+                    "$t": "Goose"
+                },
+                {
+                    "$t": "Guinea Fowl"
+                },
+                {
+                    "$t": "Kakariki"
+                },
+                {
+                    "$t": "Lory / Lorikeet"
+                },
+                {
+                    "$t": "Lovebird"
+                },
+                {
+                    "$t": "Macaw"
+                },
+                {
+                    "$t": "Ostrich"
+                },
+                {
+                    "$t": "Parakeet (Other)"
+                },
+                {
+                    "$t": "Parrot (Other)"
+                },
+                {
+                    "$t": "Parrotlet"
+                },
+                {
+                    "$t": "Peacock / Peafowl"
+                },
+                {
+                    "$t": "Pheasant"
+                },
+                {
+                    "$t": "Pigeon"
+                },
+                {
+                    "$t": "Pionus"
+                },
+                {
+                    "$t": "Poicephalus / Senegal"
+                },
+                {
+                    "$t": "Quail"
+                },
+                {
+                    "$t": "Quaker Parakeet"
+                },
+                {
+                    "$t": "Rhea"
+                },
+                {
+                    "$t": "Ringneck / Psittacula"
+                },
+                {
+                    "$t": "Rosella"
+                },
+                {
+                    "$t": "Swan"
+                },
+                {
+                    "$t": "Toucan"
+                },
+                {
+                    "$t": "Turkey"
+*/
+export default App;
